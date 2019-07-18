a61ba9810dc002a1a9011c8c46ddc457ff32e04b
Jacob <jacobphillipashe@googlemail.com>
2019-07-18 14:03:15 +0100

    improve single article css for mobile

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -26,11 +26,7 @@ class Article extends Component {
             ? "loading..."
             : this.state.comments.map(comment => {
                 return (
-                  <li
-                    key={comment.comment_id}
-                    className="articleContainer"
-                    style={{ marginRight: "1em" }}
-                  >
+                  <li key={comment.comment_id}>
                     <h6 style={{ marginLeft: "1em" }}>{comment.author}</h6>
                     <h4>{comment.body}</h4>
                   </li>
