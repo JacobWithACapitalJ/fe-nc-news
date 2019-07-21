decb854f8eba107d638ae32b052da4cbe1f3e857
Jacob <jacobphillipashe@googlemail.com>
2019-07-21 17:16:35 +0100

    implement user feedback when posting comment

--- src/components/NewComment.jsx
+++ src/components/NewComment.jsx
@@ -1,38 +1,67 @@
-import { Comment, Avatar, Form, Button, List, Input, Divider } from "antd";
+import { Comment, Avatar, Form, Button, Input, Divider } from "antd";
 import React, { Component } from "react";
-import { postComment, checkAuth } from "../utils/api";
+import { postComment } from "../utils/api";
 import cookie from "react-cookies";
 const { TextArea } = Input;
 class NewComment extends Component {
-  state = { body: "", username: this.props.author, submitted: false };
+  state = {
+    body: "",
+    username: this.props.author,
+    submitted: false,
+    submittedComment: {}
+  };
   render() {
     return (
       <span>
-        <Divider dashed />
-        <Comment
-          actions={[]}
-          author={
-            this.props.author ? this.props.author : "You're not logged in!"
-          }
-          avatar={<Avatar style={{ backgroundColor: "#87d068" }} icon="user" />}
-          // eslint-disable-next-line react/jsx-no-duplicate-props
-          actions={[
-            <Button type="primary" onClick={this.handleSubmit}>
-              New Comment
-            </Button>
-          ]}
-          content={
-            <Form>
-              <div style={{ margin: "24px 0" }} />
-              <TextArea
-                placeholder="New comment!"
-                autosize={{ minRows: 2, maxRows: 6 }}
-                onChange={this.handleChange}
-                value={this.state.body}
-              />
-            </Form>
-          }
-        />
+        {this.state.submitted ? (
+          <span>
+            <Divider dashed />
+            <Comment
+              actions={[]}
+              author={this.props.author}
+              avatar={
+                <Avatar style={{ backgroundColor: "#87d068" }} icon="user" />
+              }
+              content={this.state.submittedComment.body}
+              datetime={this.state.submittedComment.created_at}
+            />
+          </span>
+        ) : (
+          <span>
+            <Divider dashed />
+            <Comment
+              actions={[]}
+              author={
+                this.props.author ? this.props.author : "You're not logged in!"
+              }
+              avatar={
+                <Avatar style={{ backgroundColor: "#87d068" }} icon="user" />
+              }
+              // eslint-disable-next-line react/jsx-no-duplicate-props
+              actions={[
+                <Button
+                  type="primary"
+                  onClick={this.handleSubmit}
+                  disabled={this.props.author ? false : true}
+                >
+                  New Comment
+                </Button>
+              ]}
+              content={
+                <Form>
+                  <div style={{ margin: "24px 0" }} />
+                  <TextArea
+                    placeholder="New comment!"
+                    autosize={{ minRows: 2, maxRows: 6 }}
+                    onChange={this.handleChange}
+                    value={this.state.body}
+                    disabled={this.props.author ? false : true}
+                  />
+                </Form>
+              }
+            />
+          </span>
+        )}
       </span>
     );
   }
@@ -48,7 +77,12 @@ class NewComment extends Component {
       if (res.status !== 201) {
         this.setState({ author: "", body: "", submitted: false });
       } else {
-        this.setState({ author: "", body: "", submitted: true });
+        this.setState({
+          author: "",
+          body: "",
+          submitted: true,
+          submittedComment: res.data[0]
+        });
       }
     });
   };
